2da1dada3babd86f2df773fbeff8abffc3d9349c
Koregano73 <ctcwong73@gmail.com>
2022-10-26 13:06:40 -0700

    migrated Login to TS

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -1,20 +1,16 @@
 import React, { useState } from 'react';
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
   Link,
   useNavigate,
 } from 'react-router-dom';
 
 export default function Login() {
-  const [username, setUserName] = useState();
-  const [password, setPassword] = useState();
-  const [message, setMessage] = useState('');
+  const [username, setUserName] = useState<string | null>(null);
+  const [password, setPassword] = useState<string | null>(null);
+  const [message, setMessage] = useState<string>('');
   const navigate = useNavigate();
-  const handleSubmit = async (event) => {
-    console.log(username, password);
-    event.preventDefault(); 
+  const handleSubmit = async (e:React.FormEvent<EventTarget>) => {
+    e.preventDefault(); 
     try {
       const userLogin = await fetch('/login', {
         method: 'POST',
@@ -39,14 +35,18 @@ export default function Login() {
   return (
     <div className='outerBox'>
       <header><strong>AniSearch Account Login</strong></header>
-      <form onSubmit={handleSubmit} >
+      <form onSubmit={void handleSubmit} >
         <label>
           Username:
-          <input type="text" placeholder='Username' onChange={event => setUserName(event.target.value)}></input>
+          <input type="text" placeholder='Username' onChange={(event:React.FormEvent<EventTarget>) => {
+            const target = event.target as typeof event.target & { value: string }
+            setUserName(target.value)}}></input>
         </label>
         <label>
           Password:
-          <input type="password" placeholder='Password' onChange={event => setPassword(event.target.value)}></input>
+          <input type="password" placeholder='Password' onChange={(event:React.FormEvent<EventTarget>) => {
+            const target = event.target as typeof event.target & { value: string }
+            setPassword(target.value)}}></input>
         </label>
         <input className='button' type="submit" value="Login"></input>
       </form>
